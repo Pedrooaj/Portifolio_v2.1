9c17611674379bee969996333e2ce4937983aa08
Pedrooaj <pedroantonio5735@gmail.com>
2024-04-11 14:32:40 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const scrollButton = document.getElementById('scrollContato');
+const scrollProjetos = document.getElementById('scrollProjetos');
+const targetDiv = document.getElementById('projetos'); 
+const scrollForma = document.getElementById("scrollFormações");
+const divForma = document.getElementById("formações");
+const scrollSobre = document.getElementById("scrollSobre");
+const divSobre = document.getElementById("carrosel-div"); 
+
+scrollButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth'
+  });
+});
+scrollProjetos.addEventListener('click', () => {
+  targetDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+scrollForma.addEventListener('click', () => {
+    divForma.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+scrollSobre.addEventListener('click', () => {
+    divSobre.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  });
